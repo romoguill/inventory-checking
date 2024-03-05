2d5254b7399d44c1e607daf6a732f83f7531fcd4
Rodrigo Moguillansky <romoguill@gmail.com>
2024-03-05 02:13:48 -0300

    Bug fix: Changed the env source of db url

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -6,7 +6,6 @@ const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 export const db =
   globalForPrisma.prisma ||
   new PrismaClient({
-    datasourceUrl: process.env.POSTGRES_URL,
     log:
       process.env.NODE_ENV === 'development'
         ? ['query', 'error', 'warn']
