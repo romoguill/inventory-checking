5e8562d3df7733a91ad425c1be87e72490f00f47
Rodrigo Moguillansky <romoguill@gmail.com>
2024-03-18 13:47:01 -0300

    Modified product fetcher to accept options for pagination

--- src/actions/products.ts
+++ src/actions/products.ts
@@ -1,17 +1,36 @@
 import { db } from '@/lib/db';
 import { getWorkingOrganization } from './organization';
 
-export const getProducts = async () => {
+interface GetProductsOptions {
+  pagination: {
+    cursor: number;
+    limit: number;
+  };
+}
+
+export const getProducts = async (options?: GetProductsOptions) => {
   const { data: currentOrganization } = await getWorkingOrganization();
 
   if (!currentOrganization)
     return { data: null, error: { message: 'Unauthorized' } };
 
-  const response = await db.product.findMany({
-    where: {
-      organizationId: currentOrganization.id,
-    },
-  });
+  if (!options) {
+    const response = await db.product.findMany({
+      where: {
+        organizationId: currentOrganization.id,
+      },
+    });
 
-  return { data: response, error: null };
+    return { data: response, error: null };
+  } else {
+    const response = await db.product.findMany({
+      where: {
+        organizationId: currentOrganization.id,
+      },
+      skip: options.pagination.cursor,
+      take: options.pagination.limit,
+    });
+
+    return { data: response, error: null };
+  }
 };
--- src/app/(protected)/dashboard/products/page.tsx
+++ src/app/(protected)/dashboard/products/page.tsx
@@ -1,13 +1,39 @@
+import { getProducts } from '@/actions/products';
 import InnerDashboardContainer from '../../_components/InnerDashboardContainer';
 import Title from '../../_components/forms/Title';
 import TableHeader from '../../_components/tables/TableHeader';
+import Image from 'next/image';
 
-function ProductsPage() {
+async function ProductsPage() {
+  const response = await getProducts({ pagination: { cursor: 2, limit: 2 } });
+
+  if (response.error) return null;
+
+  const { data: products } = response;
+
+  console.log(products);
+
   return (
     <InnerDashboardContainer>
       <Title size='lg'>Products</Title>
       <section>
         <TableHeader />
+      </section>
+      <section>
+        {products.map((product) => (
+          <div key={product.id}>
+            {product.imageUrl && (
+              <Image
+                src={product.imageUrl}
+                width={200}
+                height={200}
+                className='object-cover h-52 w-52'
+                alt={`Product ${product.name}`}
+              />
+            )}
+            <p>{product.name}</p>
+          </div>
+        ))}
       </section>
     </InnerDashboardContainer>
   );
