92ba30b15a9845c37695ab3ca83b4724bdc17003
Rodrigo Moguillansky <romoguill@gmail.com>
2024-03-20 09:14:34 -0300

    Modified schema of policies so that each organization can customize them

--- src/app/(protected)/_components/policies/PolicyCard.tsx
+++ src/app/(protected)/_components/policies/PolicyCard.tsx
@@ -1,5 +1,10 @@
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
 import { cn } from '@/lib/utils';
 import { Severity } from '@prisma/client';
+import { Ellipsis } from 'lucide-react';
 
 interface PolicyCardProps {
   policy: {
@@ -14,15 +19,22 @@ function PolicyCard({
 }: PolicyCardProps) {
   return (
     <article className='p-4 bg-dashboard-foreground/10 rounded-xl flex-grow'>
-      <h2
-        className={cn('text-xl capitalize mb-3', {
-          'text-green-500': name === Severity.LOW,
-          'text-orange-500': name === Severity.MEDIUM,
-          'text-red-500': name === Severity.HIGH,
-        })}
-      >
-        {name.toLowerCase()}
-      </h2>
+      <div className='flex items-center justify-between'>
+        <h2
+          className={cn('text-xl capitalize mb-3', {
+            'text-green-500': name === Severity.LOW,
+            'text-orange-500': name === Severity.MEDIUM,
+            'text-red-500': name === Severity.HIGH,
+          })}
+        >
+          {name.toLowerCase()}
+        </h2>
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Ellipsis />
+          </DropdownMenuTrigger>
+        </DropdownMenu>
+      </div>
       <p>
         Delta Threshold:{' '}
         <span>
